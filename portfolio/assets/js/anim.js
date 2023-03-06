2ec843bcdf9cdcb7374188e7d00afd524b162a6a
Joshua Rodgers <34038374+joshua-rodgers@users.noreply.github.com>
2023-03-05 21:51:30 -0600

    restructured and brutalized

---
+++ portfolio/assets/js/anim.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+
+
+function draw_dot(){
+  let x = Math.floor(Math.random() * 290);
+  let y = Math.floor(Math.random() * 290);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+  ctx.fillStyle = "#f54278";
+  ctx.fillRect(x,y, 10, 10);
+}
+
+setInterval(draw_dot, 1000);
