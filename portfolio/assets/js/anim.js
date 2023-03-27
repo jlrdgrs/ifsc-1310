d1e13d761cf8c4e14dbcdfa3ab374da1dfd744a6
Joshua Rodgers <105998123+jlrdgrs@users.noreply.github.com>
2023-03-26 20:48:34 -0500

    directory re-structure, new assignments

--- portfolio/assets/js/anim.js
+++ portfolio/assets/js/anim.js
@@ -4,8 +4,8 @@ var ctx = canvas.getContext('2d');
 
 
 function draw_dot(){
-  let x = Math.floor(Math.random() * 290);
-  let y = Math.floor(Math.random() * 290);
+  let x = Math.floor(Math.random() * (canvas.width - 10));
+  let y = Math.floor(Math.random() * (canvas.height - 10));
   ctx.fillStyle = "black";
   ctx.fillRect(0,0,canvas.width, canvas.height);
   ctx.fillStyle = "#f54278";
