c925511fb1bfc0a06e96df42773a9f3aded07848
Joshua Rodgers <105998123+jlrdgrs@users.noreply.github.com>
2023-05-06 22:22:50 -0500

    dragoooon fiiiiiinallllll

---
+++ portfolio/slick-demo/assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 100,
+      fade: false,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
